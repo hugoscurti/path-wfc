2fc599aec0ac966054e111992fd35843f0516a34
Hugo Scurti <hugoscurti@gmail.com>
2018-02-22 16:30:00 -0500

    Added Custom Tilemap Editor to select tiles in sceneview; Added Custom window to select pattern for specific tile;

---
+++ Assets/Editor/SelectPatternEditorWindow.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SelectPatternEditorWindow : EditorWindow
+{
+    //private Tilemap target;
+    private PathOverlapModel model;
+    private Vector2Int tilepos;
+
+    Dictionary<int, Texture2D> patterns;
+    bool updateDict = false;
+
+    public void Init(Tilemap target, PathOverlapModel model, Vector2Int tilepos)
+    {
+        //this.target = target;
+        this.model = model;
+        this.tilepos = tilepos;
+
+        if (model != null)
+            this.patterns = this.model.GetPatternsForWave(this.tilepos);
+    }
+
+    private void OnGUI()
+    {
+        if (patterns == null)
+            EditorGUI.LabelField(new Rect(10, 10, 100, 15), new GUIContent("No model loaded..."));
+
+        else if (patterns.Count == 0)
+            EditorGUI.LabelField(new Rect(10, 10, 100, 15), new GUIContent("No patterns available :("));
+
+        else
+        {
+            int x = 20, y = 20;
+
+            foreach (KeyValuePair<int, Texture2D> p in patterns)
+            {
+                if (GUI.Button(new Rect(x, y, 100, 15), new GUIContent("Select")))
+                {
+                    // Fix selected wave
+                    model.FixWave(tilepos, p.Key);
+                    model.Print();
+                    updateDict = true;
+                    break;
+                }
+                EditorGUI.DrawPreviewTexture(new Rect(x, y + 15, 100, 100), p.Value, null, ScaleMode.ScaleToFit);
+
+                x += 110;
+                if (x + 110 > position.width )
+                {
+                    y += 120;
+                    x = 20;
+                }
+            }
+
+            if (updateDict)
+            {
+                // Reset dict of patterns (Since it most likely have changed
+                patterns = this.model.GetPatternsForWave(this.tilepos);
+                updateDict = false;
+            }
+        }
+    }
+
+    private Texture2D Scale(Texture2D img, int factor)
+    {
+        Texture2D res = new Texture2D(img.width * factor, img.height * factor);
+
+        for (int x = 0; x < res.width; ++x)
+            for (int y = 0; y < res.height; ++y)
+            {
+                Color c = img.GetPixel(x / factor, y / factor);
+                res.SetPixel(x, y, c);
+            }
+
+        return res;
+    }
+}
---
+++ Assets/Editor/TilemapEditor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CustomEditor(typeof(Tilemap))]
+public class TilemapEditor : Editor
+{
+    bool mouseDown;
+
+    private void OnSceneGUI()
+    {
+        // Simulate click without dragging
+        if (Event.current.button == 0)
+        {
+            if (Event.current.type == EventType.MouseDown)
+                mouseDown = true;
+
+            if (Event.current.type == EventType.MouseDrag)
+                // Cancel the click
+                mouseDown = false;
+
+            if (Event.current.type == EventType.MouseUp && mouseDown)
+            {
+                //Debug.Log("Mouse clicked!");
+                OnClick();
+            }
+        }
+
+    }
+
+    private void OnClick()
+    {
+        Tilemap src = target as Tilemap;
+
+        Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+        Vector3Int cellPos = src.WorldToCell(worldRay.origin);
+
+        if (cellPos.x >= 0 && cellPos.y >= 0)
+        {
+            // TODO: Select Corresponding tile
+            //Debug.Log($"Cell {cellPos.x},{cellPos.y}");
+
+            // TODO: Open window to select pattern
+            SelectPatternEditorWindow window = EditorWindow.GetWindow<SelectPatternEditorWindow>();
+            window.Init(src, src.GetComponentInParent<PathOverlap>().GetModel(), new Vector2Int(cellPos.x, cellPos.y));
+        }
+
+        
+    }
+
+}
