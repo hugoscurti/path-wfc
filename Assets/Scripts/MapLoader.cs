1b10f638c31d2a27ef10462861190df876ce8411
Hugo Scurti <hugoscurti@gmail.com>
2018-03-09 12:14:48 -0500

    Added MapLoader + Editor for the new scene;

---
+++ Assets/Scripts/MapLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapLoader : MonoBehaviour
+{
+    public Tilemap Target;
+
+    [MapIterator("Resources/MapData/output")]
+    public MapSelector outputmap = new MapSelector();
+
+    private string ResourceDirectory = "MapData/Output";
+
+    /// <summary>
+    /// Load map to target
+    /// </summary>
+    public void LoadMap()
+    {
+        FileInfo file = outputmap.GetFile();
+
+        Target.ClearAllTiles();
+
+        Texture2D source = Map.LoadMap(file, ResourceDirectory);
+
+        TileUtils.PaintTexture(source, Target);
+    }
+
+    public void ClearMap()
+    {
+        Target.ClearAllTiles();
+    }
+}
