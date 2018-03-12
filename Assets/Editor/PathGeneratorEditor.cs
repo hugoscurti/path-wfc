37462567b34f156a5d9075c136cd91f9380a297d
Hugo Scurti <hugoscurti@gmail.com>
2018-03-12 11:52:17 -0400

    Edited new scene, added PathGenerator class for new scene;

---
+++ Assets/Editor/PathGeneratorEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathGenerator))]
+public class PathGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Reset"))
+        {
+            
+        }
+
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Scripts/PathGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Parameter
+{
+    public bool FixedLength = false;
+    [Range(1,100)]
+    public int Distance;
+}
+
+public class PathGenerator : MonoBehaviour {
+
+    public Parameter TopSide;
+    public Parameter RightSide;
+    public Parameter BottomSide;
+    public Parameter LeftSide;
+
+
+    public void ExecuteAlgorithm()
+    {
+        // 1. Find obstacle
+        // Any closed polygon?
+
+        // 2. Fulfill constraints
+
+        // 3. Add random variables for missing sides
+
+        // 4. Connect the sides?
+    }
+
+    private void FindObstacle()
+    {
+
+    }
+}
