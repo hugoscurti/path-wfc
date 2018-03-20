90b8bfaccdbbfcf10b649d07b603c513b3166ab3
Hugo Scurti <hugoscurti@gmail.com>
2018-03-19 21:46:34 -0400

    Added FixedStack and Mask classes for future usage;

---
+++ Assets/Scripts/Model/FixedStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class FixedStack<T>
+{
+    T[] stack;
+
+    // This represents the size of the stack and the next index on which to stack up
+    int size;
+
+    public FixedStack(int size) {
+        stack = new T[size];
+        size = 0;
+    }
+
+    public void Push(T newval)
+    {
+        stack[size] = newval;
+        ++size;
+    }
+
+    public T Pop()
+    {
+        var elem = stack[size - 1];
+        size--;
+        return elem;
+    }
+
+    public void Clear() {
+        size = 0;
+    }
+
+    public bool IsEmpty()
+    {
+        return size == 0;
+    }
+}
---
+++ Assets/Scripts/Model/Mask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Mask
+{
+    // Unique index related to the mask
+    public int index;
+
+    // The type of mask.
+    // (Either everything except the colors defined in set or simply the colors in set)
+    public bool everythingExcept;
+
+    // The colors that it accepts
+    public HashSet<int> colors;
+
+
+    // TODO: add utility functions to compare mask with color, or others...
+
+}
